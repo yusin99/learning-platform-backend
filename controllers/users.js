9d7ecacc5ff6805a0abbb280dc38e1bc94c64162
Yusin <sunkatabeckama@gmail.com>
2022-04-09 12:06:30 +0200

    Adding users controller

---
+++ controllers/users.js
@@ -0,0 +1,25 @@
+const getAllUsers = async (req,res)=>{
+    res.send("All users")
+}
+const getUser = async (req,res)=>{
+    res.send("One user")
+}
+const createUser = async (req,res)=>{
+    res.send("Create user")
+}
+
+const updateUser = async (req,res)=>{
+    res.send("Update user")
+}
+
+const deleteUser = async (req,res)=>{
+    res.send("Delete user")
+}
+
+module.exports = {
+    getAllUsers,
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser
+}
