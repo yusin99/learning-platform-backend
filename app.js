ef52ffd746c7a04a7c4c74f5b1a98d2b1f74bc4b
Yusin <sunkatabeckama@gmail.com>
2022-04-09 13:57:51 +0200

    App.js configurations => defining routes + connection to the DB

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// connectDB
+const connectDB = require('./db/connect')
+// routers
+const authRouter = require('./routes/auth')
+const usersRouter = require('./routes/users')
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -11,9 +17,8 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('Learning platform api');
-});
+app.use('/api/v1/auth',authRouter)
+app.use('/api/v1/users',usersRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -22,6 +27,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
