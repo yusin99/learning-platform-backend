1b6272435880dbec8fcd77ebac90b7aa7dd1d630
Yusin <sunkatabeckama@gmail.com>
2022-04-15 22:23:50 +0200

    Sining Json Web Token to every newly registered user

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-  const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", {
+  const token = jwt.sign({ userId: user._id, name: user.name }, process.env.jwtSecret, {
     expiresIn: "30d",
   });
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -33,8 +34,14 @@ UserSchema.pre("save", async function (next) {
   next();
 });
 
-UserSchema.methods.getName = function () {
-  return this.name;
+UserSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.jwtSecret,
+    {
+      expiresIn: "30d",
+    }
+  );
 };
 
 module.exports = mongoose.model("User", UserSchema);
